9856e54b08ae4199b8d3e3ec32fdaf45805453ce
smukh002 <46467209+smukh002@users.noreply.github.com>
2020-12-21 12:54:08 -0800

    Add files via upload

---
+++ CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour{
+    // movement amount trying to capture
+    [SerializeField] private Vector3 move;
+    // target we are trying to follow
+    [SerializeField] private Transform car;
+    [SerializeField] private float xspeed;
+    [SerializeField] private float rspeed;
+
+    private void FixedUpdate(){
+        Translation();
+        Rotation();
+    }
+   
+    private void Translation(){
+        var target = car.TransformPoint(move);
+        //Vector3 Lerp(Vector3 a, Vector3 b, float t);
+        float t = xspeed * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, target, t);
+    }
+    private void Rotation(){
+        var direction = car.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        float z = rspeed * Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, z);
+    }
+}
